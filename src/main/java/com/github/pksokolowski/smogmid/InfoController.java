03f44ef72ec791ebf4f99956000ad69b46db6705
Paweł Sokołowski <p.k.sokolowski@outlook.com>
2019-01-03 23:55:48 +0100

    improve info shown
    When user navigates to /info, they now receive a better list of stations
    along with index levels at those stations. Additionally links to google
    maps were added so clicking on a given station user can immediately
    check its physical location on the map.

--- src/main/java/com/github/pksokolowski/smogmid/InfoController.java
+++ src/main/java/com/github/pksokolowski/smogmid/InfoController.java
@@ -5,6 +5,8 @@ import com.github.pksokolowski.smogmid.repository.AirQualityLogsRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class InfoController {
 
@@ -19,7 +21,7 @@ public class InfoController {
         var logs = aqLogsRepository.findAll();
 
         StringBuilder sb = new StringBuilder();
-        sb.append("<body bgcolor=\"#000000\" text=\"white\"/>");
+        sb.append("<body bgcolor=\"#000000\" text=\"white\" link=\"white\" vlink=\"beige\"/>");
         sb.append("Info page");
         sb.append("<br>Logs count: ").append(logs.size());
 
@@ -43,9 +45,18 @@ public class InfoController {
                 sb.append(String.format("<br> %d : %d%%", i, percentage));
             }
         }
-        sb.append("<br><br><hr>");
+        sb.append("<br><br><hr>Data from individual stations (click at a station to see it on a map)");
         for (AirQualityLog log : logs) {
-            sb.append(log.getDetails().encode()).append(" , ");
+            var id = log.getStationId();
+            var index = log.getDetails().getHighestIndex();
+            var details = log.getDetails().encode();
+            var location = log.getLocation();
+            var mapAddress = String.format(Locale.US,
+                    "https://www.google.com/maps/search/?api=1&query=%f,%f",
+                    location.getLatitude(),
+                    location.getLongitude());
+
+            sb.append(String.format("<br> <a href = %s>%d: <b>%d</b> &nbsp;(%d)</a>", mapAddress, id, index, details));
         }
 
         return sb.toString();
